fc97603c725e1be148e21433caaa9dc5088324e8
Wenzel, Toni <toni.wenzel@chg-meridian.com>
2025-01-07 17:39:25 +0100

    New git global

---
+++ src/GitGlobal.cs
@@ -0,0 +1,20 @@
+﻿namespace HansPeterGit;
+
+/// <summary>
+/// Helper class for global Git operations
+/// </summary>
+public static class GitGlobal
+{
+    /// <summary>
+    /// Changes the global git configuration
+    /// </summary>
+    /// <param name="configName">Name of the configuration entry</param>
+    /// <param name="value">The new value</param>
+    /// <param name="options">The git options.</param>
+    public static void Config(string configName, string value, GitOptions options)
+    {
+        var helperWithoutWorkdir = new GitHelper(options with { WorkingDirectory = string.Empty });
+
+        helperWithoutWorkdir.Command("config", "--global", configName, value);
+    }
+}
